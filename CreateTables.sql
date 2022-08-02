0558ca931080527b6de5f5e1800afa68ef163721
enio-martinelli <75862508+enio-martinelli@users.noreply.github.com>
2022-08-02 15:26:01 -0300

    Create CreateTables.sql
    Criação do BD; 
    Criação das tabelas: 'professor', 'pro_reitoria' e 'pro_reitoria_professor'.

---
+++ CreateTables.sql
@@ -0,0 +1,33 @@
+
+CREATE TABLE professor(
+    id_prof smallserial PRIMARY KEY,
+    nome varchar not null,
+    email varchar not null,
+    titulo varchar not null
+);
+
+CREATE TABLE pro_reitoria(
+    sigla_pro_reitoria varchar(15) not null PRIMARY KEY,
+    localizacao geometry,
+    nome varchar not null
+);
+
+CREATE TABLE pro_reitoria_professor(
+    id_prof integer not null,
+    sigla_pro_reitoria varchar(15) not null,
+    período varchar not null,
+    cargo varchar not null,
+    foreign key(id_prof) references professor(id_prof),
+    foreign key(sigla_pro_reitoria) references pro_reitoria(sigla_pro_reitoria)
+);
+
+
+-- SCHEMA: createTables
+
+-- DROP SCHEMA IF EXISTS "createTables" ;
+
+CREATE SCHEMA IF NOT EXISTS "createTables"
+    AUTHORIZATION postgres;
+
+COMMENT ON SCHEMA "createTables"
+    IS 'Criação de tabelas';
