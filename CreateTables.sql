65bbcabd26d73d07b515489ff1dc6be51a39f98a
audreihferreira <85792898+audreihferreira@users.noreply.github.com>
2022-08-02 22:29:39 -0300

    Update CreateTables.sql

--- CreateTables.sql
+++ CreateTables.sql
@@ -30,6 +30,51 @@ CREATE TABLE reitoria_professor(
     foreign key(sigla_reitoria) references reitoria(sigla_reitoria)
 );
 
+--DEPARTAMENTO
+
+CREATE TABLE departamento (
+	sigla_departamento VARCHAR NOT NULL,
+    sigla_centro VARCHAR NOT NULL,
+    nome_depart VARCHAR NOT NULL,
+    localizacao_depart GEOMETRY,
+    site_depart VARCHAR,
+    tel_depart VARCHAR NOT NULL,
+	email_depart VARCHAR NOT NULL,
+     
+	--DEFINIÇÃO DAS RESTRIÇÕES
+        --definição da restrição de chave primária
+	CONSTRAINT departamento_pk PRIMARY KEY (sigla_departamento),
+        --definição da restrição de chave estrangeira
+    CONSTRAINT departamento_fk FOREIGN KEY (sigla_centro) REFERENCES centro(sigla_centro)
+    
+);
+
+--CURSOS_GRADUAÇÃO
+
+CREATE TABLE curso_graduacao (
+	sigla_departamento VARCHAR NOT NULL,
+    nome_cg VARCHAR NOT NULL,
+     
+	--DEFINIÇÃO DAS RESTRIÇÕES
+        --definição da restrição de chave primária
+	CONSTRAINT curso_graduacao_pk PRIMARY KEY (sigla_departamento, nome_cg),
+        --definição da restrição de chave estrangeira
+    CONSTRAINT curso_graduacao_fk FOREIGN KEY (sigla_departamento) REFERENCES departamento(sigla_departamento)
+);
+
+--CURSOS_POS_GRADUAÇÃO
+
+CREATE TABLE curso_pos_graduacao (
+	sigla_departamento VARCHAR NOT NULL,
+    nome_cpg VARCHAR NOT NULL,
+     
+	--DEFINIÇÃO DAS RESTRIÇÕES
+        --definição da restrição de chave primária
+	CONSTRAINT curso_posgrad_pk PRIMARY KEY (sigla_departamento, nome_cpg),
+        --definição da restrição de chave estrangeira
+    CONSTRAINT curso_pos_grad_fk FOREIGN KEY (sigla_departamento) REFERENCES departamento(sigla_departamento)
+);
+
 -- SCHEMA: createTables
 
 -- DROP SCHEMA IF EXISTS "createTables" ;
