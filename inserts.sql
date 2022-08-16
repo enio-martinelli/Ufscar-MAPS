422724060e3013477ba14eeb4f39b5bdfd63f2b7
lsr94 <110498717+lsr94@users.noreply.github.com>
2022-08-16 19:05:24 -0300

    Update inserts.sql

--- inserts.sql
+++ inserts.sql
@@ -1,1 +1,24 @@
+--- inserções centro ---
+INSERT INTO centro (sigla_centro, reitoria_id, nome, localizacao, telefone) 
+VALUES ('CCET', 1, 'Centro de Ciências Exatas e de Tecnologia', NULL, '16 3351-8201');
 
+INSERT INTO centro (sigla_centro, reitoria_id, nome, localizacao, telefone)
+VALUES ('CCBS', 1, 'Centro de Ciências Biológicas e da Saúde', NULL, '16 3351-8301');
+
+INSERT INTO centro (sigla_centro, reitoria_id, nome, localizacao, telefone)
+VALUES ('CECH', 1, 'Centro de Educação e Ciências Humanas', NULL, '16 3351-8351');
+
+INSERT INTO centro (sigla_centro, reitoria_id, nome, localizacao, telefone)
+VALUES ('CCGT', 1, 'Centro de Ciências em Gestão e Tecnologia', NULL, '15 3229-7546');
+
+INSERT INTO centro (sigla_centro, reitoria_id, nome, localizacao, telefone)
+VALUES ('CCHB', 1, 'Centro de Ciências Humanas e Biológicas', NULL, '15 3229-7470');
+
+INSERT INTO centro (sigla_centro, reitoria_id, nome, localizacao, telefone)
+VALUES ('CCTS', 1, 'Centro de Ciências e Tecnologias para Sustentabilidade', NULL, '15 3329-5937');
+
+INSERT INTO centro (sigla_centro, reitoria_id, nome, localizacao, telefone)
+VALUES ('CCA', 1, 'Centro de Ciências Agrárias', NULL, '19 3543-2600');
+
+INSERT INTO centro (sigla_centro, reitoria_id, nome, localizacao, telefone)
+VALUES ('CCN', 1, 'Centro de Ciências da Natureza', NULL, NULL);
