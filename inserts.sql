1dad032f609e391ca65fdf1f8f5e936db98f938d
Karen Ketlyn Ferreira Barcelos <72056327+42kkkkkaren@users.noreply.github.com>
2022-08-16 23:44:44 -0300

    Update inserts.sql

--- inserts.sql
+++ inserts.sql
@@ -22,3 +22,19 @@ VALUES ('CCA', 1, 'Centro de Ciências Agrárias', NULL, '19 3543-2600');
 
 INSERT INTO centro (sigla_centro, reitoria_id, nome, localizacao, telefone)
 VALUES ('CCN', 1, 'Centro de Ciências da Natureza', NULL, NULL);
+
+--- inserções Professor ---
+INSERT INTO professor (nome, email, titulo) 
+VALUES ('Ana Beatriz de Oliveira', 'biaoliveira@ufscar.br', 'Doutora');
+
+INSERT INTO professor (nome, email, titulo) 
+VALUES ('Maria de Jesus Dutra dos Reis', 'dmjr@ufscar.br', 'Doutora');
+
+--- inserções Reitoria Professor ---
+#Profa. Dra. Ana Beatriz de Oliveira
+INSERT INTO reitoria_professor (prof_id, reitoria_id, periodo, cargo) 
+VALUES ((buscar tabela), 1, '2020 - Atual', 'Reitora',);
+
+#Profa. Dra. Maria de Jesus Dutra dos Reis
+INSERT INTO reitoria_professor (prof_id, reitoria_id, periodo, cargo) 
+VALUES ((buscar tabela), 1, '2021 - Atual', 'Vice Reitora',);
